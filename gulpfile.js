20e77b4d33dd8ed121199ea04584cd08a5fd2d5e
Stefan M. Kudwien <mail@stefankudwien.com>
2016-04-27 17:30:21 +0200

    Refactored gulp workflow for bootstrap subthemes

--- gulpfile.js
+++ gulpfile.js
@@ -7,76 +7,77 @@ var gulp = require('gulp'),
   lazypipe = require('lazypipe'),
   reporter = require("postcss-reporter"),
   // @todo Rename production server environment variable ;)
-  is_production = process.env.COMPASS_PRODUCTION === 'true',
-  path = require('path'),
-  theme_path = path.join(__dirname, 'htdocs', 'themes', '{{- theme }}');
+  isProduction = process.env.COMPASS_PRODUCTION === 'true',
+  themePath = 'htdocs/themes/empfehlung';
 
-gulp.task('styles', function () {
 
-  var csslintTasks = lazypipe()
-    // @todo Custom reporter to $.notify() user if any problems
-    .pipe($.csslint, 'htdocs/.csslintrc')
-    .pipe($.csslint.reporter);
+// Common gulp-plumber error handler options
+var plumberOptions = {
+  errorHandler: $.notify.onError({
+    message: 'Error: <%- error.message %>',
+    title: '<%- error.plugin %>'
+  })
+};
 
-  return gulp.src(path.join(theme_path, 'less', 'style.less'))
-    // Catch any errors to prevent them from crashing gulp
-    .pipe($.plumber({
-      errorHandler: $.notify.onError({
-        message: 'Error: <%- error.message %>',
-        title: '<%- error.plugin %>'
-      })
-    }))
-    //.pipe($.debug())
-    // Start source maps processing
-    .pipe($.if(!is_production, $.sourcemaps.init()))
-    // Convert scss to css
-    .pipe($.less({
-      paths: [ theme_path, 'less' ]
-    }))
-    .pipe($.postcss([
-      lost(),
-      autoprefixer({
-        browsers: ['> 1% in DE', '> 1% in AT', '> 1% in CH', 'last 2 versions', 'Firefox ESR']
-      }),
-      reporter({ clearMessages: true })
-    ]))
-    // Write source maps
-    .pipe($.if(!is_production, $.sourcemaps.write()))
-    // Lint css using Drupal rules
-    // @todo Find a way to not use linting on bootstrap css
-    //.pipe($.if(!is_production, csslintTasks()))
-    // Write to destination
-    .pipe(gulp.dest(path.join(theme_path, 'css')))
-    // Push to livereload
-    .pipe($.if(!is_production, $.livereload()));
+
+// LESS pipeline
+var lessTasks = lazypipe()
+  //.pipe($.debug)
+  .pipe(function() { return $.if(!isProduction, $.sourcemaps.init()); })
+  .pipe($.less, { paths: [themePath + '/less'] })
+  .pipe($.postcss, [
+    lost(),
+    autoprefixer({
+      browsers: ['> 1% in DE', '> 1% in AT', '> 1% in CH', 'last 2 versions']
+    }),
+    reporter({ clearMessages: true })
+  ])
+  .pipe(function() { return $.if(!isProduction, $.sourcemaps.write()); })
+  // @todo Find a way to not use linting on bootstrap css
+  //.pipe(function() { return $.if(!isProduction, $.csslint('htdocs/.csslintrc')); })
+  .pipe(function() { return $.if(!isProduction, $.csslint.reporter()); });
+
+
+gulp.task('styles:theme', function() {
+  // Store compiled bootstrap subtheme styles in separate directory
+  return gulp.src(themePath + '/less/style.less')
+    .pipe($.plumber(plumberOptions))
+    .pipe(lessTasks())
+    .pipe(gulp.dest(themePath + '/css'))
+    .pipe($.if(!isProduction, $.livereload()));
 });
 
-gulp.task('scripts', function () {
 
-  var eslintTasks = lazypipe()
-    // @todo Custom reporter to $.notify() user if any problems
-    .pipe($.eslint)
-    .pipe($.eslint.format);
-
-  return gulp.src('htdocs/{modules,themes}/**/*.js', { base: 'htdocs' })
-    // Filter out third-party and minified scripts before linting
-    .pipe($.filter(['**/*', '!**/{contrib,vendor}/**', '!**/*.min.js']))
-    // Catch any errors to prevent them from crashing gulp
-    .pipe($.plumber({
-      errorHandler: $.notify.onError({
-        message: 'Error: <%- error.message %>',
-        title: '<%- error.plugin %>'
-      })
-    }))
-    .pipe($.uglify())
-    .pipe($.rename({
-      extname: '.min.js'
-    }))
+gulp.task('styles:modules', function() {
+  // Store compiled custom modules styles alongside LESS files
+  return gulp.src('htdocs/modules/custom/**/*.less', { base: 'htdocs' })
+    .pipe($.plumber(plumberOptions))
+    .pipe(lessTasks())
     .pipe(gulp.dest('htdocs'))
-    //.pipe($.if(!is_production, eslint()))
-    // Push to livereload
-    .pipe($.if(!is_production, $.livereload()));
+    .pipe($.if(!isProduction, $.livereload()));
 });
+
+
+var scriptsGlob = [
+  themePath + '/js/**/*.js',
+  'htdocs/modules/custom/**/*.js'
+];
+
+gulp.task('scripts', function() {
+  // Store compiled custom modules scripts alongside their source files
+  return gulp.src(scriptsGlob, { base: 'htdocs' })
+    // Exclude third-party and minified scripts
+    .pipe($.filter(['**/*', '!**/{contrib,vendor}/**', '!**/*.min.js']))
+    .pipe($.plumber(plumberOptions))
+    //.pipe($.debug())
+    .pipe($.uglify())
+    //.pipe($.if(!isProduction, $.eslint()))
+    .pipe($.if(!isProduction, $.eslint.format()))
+    .pipe($.rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('htdocs'))
+    .pipe($.if(!isProduction, $.livereload()));
+});
+
 
 //gulp.task('images', function () {
 //  return gulp.src('htdocs/{modules,themes}/**/*.{png,jpg,jpeg,gif}', { base: 'htdocs' })
@@ -88,28 +89,15 @@ gulp.task('scripts', function () {
 //    .pipe(gulp.dest('htdocs'));
 //});
 
-gulp.task('build', function () {
-  gulp.start('styles');
-  //gulp.start('scripts');
-  //gulp.start('images');
-});
 
-gulp.task('watch', function () {
+gulp.task('build', ['styles:theme', 'styles:modules', 'scripts'/*, 'images'*/]);
+
+gulp.task('watch', function() {
   $.livereload.listen();
-
-  // Watch for modifications and run tasks
-  gulp.watch('htdocs/{modules,themes}/**/*.less', ['styles']);
-  //gulp.watch('htdocs/{modules,themes}/**/*.js', ['scripts']);
+  gulp.watch('htdocs/themes/**/*.less', ['styles:theme']);
+  gulp.watch('htdocs/modules/custom/**/*.less', ['styles:modules']);
+  gulp.watch(scriptsGlob, ['scripts']);
   //gulp.watch('htdocs/{modules,themes}/**/*.{png,jpg,jpeg,gif}', ['images']);
 });
 
-gulp.task('clean', function (done) {
-  //gulp.src('htdocs/{modules,themes}/**/*.scss', { read: false, base: 'htdocs' })
-  //  @todo Delete generated .css files, but no other
-  //  .pipe();
-  //return $.cache.clearAll(done);
-});
-
-gulp.task('default', ['build'], function () {
-  gulp.start('watch');
-});
+gulp.task('default', ['build', 'watch']);
